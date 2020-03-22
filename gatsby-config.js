503059200200b0af0d569fdd548ba6404c6fe383
benyou1969 <benyou.11@hotmail.com>
2020-03-22 23:25:40 +0100

    remove purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,17 +81,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-postcss`,
-    {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        printRejected: true, // Print removed selectors and processed file names
-        develop: false, // Enable while using `gatsby develop`
-        tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-      },
-    },
+   
     "gatsby-plugin-svgr",
     {
       resolve: `gatsby-plugin-typography`,
